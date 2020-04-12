9ee91c11325ca820bf31b5c8e32918e9ec6472f2
suresh <katikalasureshkumar@gmail.com>
2020-04-12 09:37:49 +0530

    code with mohfw

--- src/App.js
+++ src/App.js
@@ -1,38 +1,55 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Map from './Map.js'
 const cheerio = require('cheerio')
 const axios = require('axios')
 
-function App() {
+class App extends React.Component{
+  // const [statesData, setObj] = useState()
+  constructor(props) {
+    super(props);
+    this.state = {
+      statesData: []
+    };
+  }
+  // let scrapedData = [];
+  // useEffect(() => {
+    componentDidMount() {
+      let scrapedData = []
 
-  axios.get('https://www.mohfw.gov.in/').then((response) => {
-  // Load the web page source code into a cheerio instance
-  const $ = cheerio.load(response.data)
-  console.log($)
-})
-
+    axios.get('https://www.mohfw.gov.in/').then((response) => {
+      // Load the web page source code into a cheerio instance
+      const $ = cheerio.load(response.data)
+      // $("body > div:nth-child(3) > div > div > div > ol > strong > strong > strong > strong > div > table > tbody > tr > td").each((index, element) => {
+      //   console.log($(element).text());
+      // document.querySelector('body > div:nth-child(3) > div > div > div > ol > strong > strong > strong > strong > div > table > tbody > tr:nth-child(1)')
+      // });
+      $('body > div:nth-child(3) > div > div > div > ol > strong > strong > strong > strong > div > table > tbody > tr').each((index, element) => {
+        const tds = $(element).find("td");
+        const serial = $(tds[0]).text();
+        const stateName = $(tds[1]).text();
+        const indConfirmed = $(tds[2]).text();
+        const foreignConfirmed = $(tds[3]).text();
+        const cured = $(tds[4]).text();
+        const deaths = $(tds[5]).text();
+        const tableRow = { serial, stateName, indConfirmed, foreignConfirmed, cured, deaths };
+        scrapedData.push(tableRow);
+      });
+      console.log(scrapedData)
+      this.setState({statesData : scrapedData})
+      // return scrapedData
+    })
+      // .then(setObj)
+  // }, [])
+    }
+render() {
   return (
     <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
-      <Map />
-
+      <Map statesData={this.state.statesData} />
     </div>
   );
+}
 }
 
 export default App;
--- src/Map.js
+++ src/Map.js
@@ -1,26 +1,49 @@
 import * as React from 'react'
- 
 import DatamapsIndia from 'react-datamaps-india'
  
-const Map = () => {
+const Map = ({statesData}) => {
+  console.log("states Data : ",statesData)
+  let regionDataObject = {};
+  if(statesData.length >0) {
+  statesData.forEach(el => {
+    regionDataObject[el.stateName] = {
+      value : el.indConfirmed
+    }
+  })
+  regionDataObject.Telangana = regionDataObject.Telengana
+  console.log(regionDataObject)
+  regionDataObject['Jammu & Kashmir'] = {}
+  regionDataObject['Jammu & Kashmir'].value = parseInt(regionDataObject['Union Territory of Jammu and Kashmir'].value) + parseInt(regionDataObject['Union Territory of Ladakh'].value)
+
+
+}
   return (
     <DatamapsIndia
-      regionData={{
-        Maharashtra: {
-          value: 10
-        }
-      }}
+      // regionData={{
+      //   Maharashtra: {
+      //     value: 10
+      //   }
+      // }}
+      regionData={regionDataObject}
       mapLayout={{
         title: '',
         legendTitle: '',
-        startColor: 'blue',
+        startColor: '#FFFFCC',
         endColor: 'red',
         hoverTitle: 'Count',
         noDataColor: '#f5f5f5',
         borderColor: '#8D8D8D',
         hoverBorderColor: '#8D8D8D',
-        hoverColor: 'green'
+        hoverColor: '#D3D3D3'
       }}
+      hoverComponent={({ value }) => {
+        return (
+          <>
+            <p>{value.name}</p>
+            <p>{value.value}</p>
+          </>
+        )
+        }}
     />
   )
 }
